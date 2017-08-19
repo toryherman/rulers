c8bc3067017458966432972b1916781f163f5a4f
Tory Herman <tory.herman12@gmail.com>
2017-08-19 16:58:18 -0500

    add controller

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,11 +1,39 @@
 require "rulers/version"
-require "rulers/array"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
       `echo debug > debut.txt`;
-      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+
+      if env["PATH_INFO"] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env["PATH_INFO"] == "/"
+        # temporary rerouting to google
+        return [302, {'Location' => "http://google.com"}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+
+      begin
+        text = controller.send(act)
+      rescue
+        # what to do if action doesn't exist
+        text = "ERROR"
+      end
+
+      [200, {'Content-Type' => 'text/html'}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
--- rulers.gemspec
+++ rulers.gemspec
@@ -32,7 +32,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.13"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rack-test"
-  spec.add_development_dependency "test-unit"
-  spec.add_runtime_dependency "rack"
+  spec.add_development_dependency "rack-test", "~> 0.6"
+  spec.add_development_dependency "test-unit", "~> 3.1"
+  spec.add_runtime_dependency "rack", "~> 2.0"
 end
