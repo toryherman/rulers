5f9bea277a0a511eb50121e0feb51826ad5af068
Tory Herman <tory.herman12@gmail.com>
2017-08-27 18:45:17 -0500

    add routing

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -12,21 +12,10 @@ module Rulers
 
       if env["PATH_INFO"] == '/favicon.ico'
         return [404, {'Content-Type' => 'text/html'}, []]
-      elsif env["PATH_INFO"] == "/"
-        # temporary rerouting to google
-        return [302, {'Location' => "http://google.com"}, []]
       end
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      r = controller.get_response
-
-      if r
-        [r.status, r.headers, [r.body].flatten]
-      else
-        [200, {'Content-Type' => 'text/html'}, [text]]
-      end
+      rack_app = get_rack_app(env)
+      rack_app.call(env)
     end
   end
 end
--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -1,6 +1,5 @@
 require "erubis"
 require "rulers/file_model"
-require "rulers/view"
 require "rack/request"
 
 module Rulers
@@ -9,40 +8,34 @@ module Rulers
 
     def initialize(env)
       @env = env
+      @routing_params = {}
     end
 
     def env
       @env
     end
 
-    def instance_hash
-      h = {}
-      instance_variables.each do |i|
-        h[i] = instance_variable_get i
-      end
-      h
-    end
-
-    def render(view_name, locals = {})
-      filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
-      template = File.read filename
-      v = View.new
-      v.set_vars instance_hash
-      v.evaluate template
-    end
-
-    def controller_name
-      klass = self.class
-      klass = klass.to_s.gsub /Controller$/, ""
-      Rulers.to_underscore klass
-    end
-
     def request
       @request ||= Rack::Request.new(@env)
     end
 
+    def dispatch(action, routing_params = {})
+      @routing_params = routing_params
+      text = self.send(action)
+      r = get_response
+      if r
+        [r.status, r.headers, [r.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text].flatten]
+      end
+    end
+
+    def self.action(act, rp = {})
+      proc { |e| self.new(e).dispatch(act, rp) }
+    end
+
     def params
-      request.params
+      request.params.merge @routing_params
     end
 
     def response(text, status = 200, headers = {})
@@ -57,6 +50,19 @@ module Rulers
 
     def render_response(*args)
       response(render(*args))
+    end
+
+    def render(view_name, locals = {})
+      filename = File.join "app", "views", controller_name, "#{view_name}.html.erb"
+      template = File.read filename
+      eruby = Erubis::Eruby.new(template)
+      eruby.result locals.merge(:env => env)
+    end
+
+    def controller_name
+      klass = self.class
+      klass = klass.to_s.gsub /Controller$/, ""
+      Rulers.to_underscore klass
     end
   end
 end
